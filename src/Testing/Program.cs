1f92e535a4ed2e2e02a6ee3b472ced9192e06423
FireHeart003 <65786491+FireHeart003@users.noreply.github.com>
2026-09-25 15:34:58 -0400

    A lot of code

--- src/Testing/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using Xunit;
-
-var a = new ShowSummary(Guid.Empty, "Severance");
-var b = new ShowSummary(Guid.Empty, "Severance");
-
-Assert.True(a == b);          // ?
-Assert.False(ReferenceEquals(a, b));  // ?
-public record ShowSummary(Guid Id, string Title);
